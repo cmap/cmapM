a1ba35b2f20019bfed6b876294fcaa7c50d97390
Rajiv Narayan <narayan@boradinstitute.org>
2017-05-02 16:47:25 -0400

    Fixed order of intialization issue in setup_env

--- setup_env.m
+++ setup_env.m
@@ -3,6 +3,11 @@
 % Obtain absolute path to this script
 this_path = fileparts(mfilename('fullpath'));
 
+% Environment variable with absolute path to the cmapM base folder
+% Referenced within the library using the command cmapmpath
+setenv('CMAPMPATH', this_path);
+fprintf(1, 'Added cmapM to the search path\n');
+
 % Add appropriate paths to matlab environment
 addpath(genpath(this_path));
 addpath(genpath(fullfile(this_path, 'bin')));
@@ -10,7 +15,3 @@ addpath(genpath(fullfile(this_path, 'bin')));
 % Download and unpack assets if not already present
 download_cmapm_assets;
 
-% Environment variable with absolute path to the cmapM base folder
-% Referenced within the library using the command cmapmpath
-setenv('CMAPMPATH', this_path);
-fprintf(1, 'Added cmapM to the search path\n');
